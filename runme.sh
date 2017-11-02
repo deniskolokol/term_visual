3211de532690b908d1b2ba742a6bedd5fb0f939c
Denis Kolokol <dkolokol@gmail.com>
2017-11-02 11:32:39 +0100

    live visuals setup

--- runme.sh
+++ runme.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-tmux new-session -d '~/Documents/OpenNI/OSCeleton/osceleton -a 192.168.1.132 -p 57120 | ./listener.py'
+tmux new-session -d './fakinect.py'
 tmux split-window -h './display.py'
 tmux split-window -v 'htop -d 2'
 tmux split-window -v 'top'
+tmux select-pane -L
+tmux split-window -v '~/Documents/OpenNI/OSCeleton/osceleton -a 192.168.1.132 -p 57120'
+tmux resize-pane -D 15
+tmux select-pane -R
+tmux select-pane -U
 tmux -2 attach-session -d
--- runtrace.sh
+++ runtrace.sh
@@ -4,4 +4,7 @@ tmux new-session -d './fakinect.py'
 tmux split-window -h './display.py'
 tmux split-window -v 'htop -d 2'
 tmux split-window -v 'top'
+tmux select-pane -L
+tmux split-window -v './fakinect.py' # '~/Documents/OpenNI/OSCeleton/osceleton -a 192.168.1.132 -p 57120'
+tmux resize-pane -D 15
 tmux -2 attach-session -d
